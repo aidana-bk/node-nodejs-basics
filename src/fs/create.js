7633aab33212b766c2f13723afb35c0eefbe7f28
Aidana K <aidana.amnbk@gmail.com>
2025-04-26 20:39:58 +0500

    feat: fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = `fs/files/fresh.txt`;
+  try {
+    await fs.access(filePath);
+    throw new Error("FS operation failed.");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile(filePath, "I am fresh and young");
+      console.log("fresh.txt was created successfully :)");
+    } else {
+      console.error("Error: ", err);
+    }
+  }
 };
 
 await create();
