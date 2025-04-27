281b9de4aed5b4a4ee65794122745d3f74fee2c6
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 17:15:03 +0500

    feat: cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = [];
+  // console.log(process.argv);
+  for (let i = 2; i < process.argv.length; i += 2) {
+    const key = process.argv[i].replace("--", "");
+    const value = process.argv[i + 1];
+    const formattedString = `${key} is ${value}`;
+    args.push(formattedString);
+  }
+  console.log(args.join(", "));
 };
 
 parseArgs();
