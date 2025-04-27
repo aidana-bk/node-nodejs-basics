ccd3465246aa82780e692cce4872fff330af9d27
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 16:14:48 +0500

    feat: fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = `fs/files/fileToRemove.txt`;
+  try {
+    await fs.unlink(filePath);
+  } catch (err) {
+    console.error("FS operation failed");
+  }
 };
 
 await remove();
