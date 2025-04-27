94f2a1b08410b37e3778ee22a0f61bbd99f839e6
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 20:42:52 +0500

    feat: steams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = "src/streams/files/fileToWrite.txt";
+  try {
+    const writableStream = fs.createWriteStream(filePath);
+    process.stdin.pipe(writableStream);
+    writableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    writableStream.on("finish", () => {
+      console.log("Finish");
+    });
+    writableStream.on("close", () => {
+      console.log("Closing stream");
+    });
+    process.stdin.on("error", (err) => {
+      console.error(err.message);
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await write();
