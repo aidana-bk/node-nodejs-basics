abf06027cdbd4d87efb172a3d8cdb1f516d0e600
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 20:42:45 +0500

    feat: steams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = "src/streams/files/fileToRead.txt";
+  try {
+    await fs.promises.access(filePath);
+    const readableStream = fs.createReadStream(filePath);
+    readableStream.pipe(process.stdout);
+    readableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    readableStream.on("end", () => {
+      console.log("Finish");
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await read();
