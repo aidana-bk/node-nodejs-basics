56672f2060ec6de6be31a5018615e5f7c2c8480e
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 16:26:58 +0500

    feat: fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const filePath = `src/fs/files`;
+  fs.readdir(filePath)
+    .then((files) => {
+      console.log(files);
+    })
+    .catch((err) => {
+      console.error("FS operation failed");
+    });
 };
 
 await list();
