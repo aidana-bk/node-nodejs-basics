f8506a8dac1ae516ad111aceb27fde347f407a46
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 16:22:26 +0500

    feat: fs/rename.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,13 +4,13 @@ const create = async () => {
   const filePath = `fs/files/fresh.txt`;
   try {
     await fs.access(filePath);
-    throw new Error("FS operation failed.");
+    throw new Error("FS operation failed");
   } catch (err) {
     if (err.code === "ENOENT") {
       await fs.writeFile(filePath, "I am fresh and young");
       console.log("fresh.txt was created successfully :)");
     } else {
-      console.error("Error: ", err);
+      console.error("FS operation failed");
     }
   }
 };
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  const filepath = "fs/files";
+  try {
+    await fs.access(`${filepath}/properFilename.md`);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      fs.rename(
+        `${filepath}/wrongFilename.txt`,
+        `${filepath}/properFilename.md`
+      )
+        .then(() => {
+          console.log("File renamed successfully.");
+        })
+        .catch((err) => {
+          console.error("FS operation failed");
+        });
+    } else {
+      console.error("FS operation failed");
+    }
+  }
 };
 
 await rename();
