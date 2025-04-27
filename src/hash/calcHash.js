c27ed314025886e974922cf7fdc7ad5519593ad1
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 19:51:56 +0500

    feat: hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = "src/hash/files/fileToCalculateHashFor.txt";
+  const hash = crypto.createHash("sha256");
+  const fileStream = fs.createReadStream(filePath);
+  fileStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+  fileStream.on("end", () => {
+    const fileHash = hash.digest("hex");
+    console.log(fileHash);
+  });
+  fileStream.on("error", (err) => {
+    console.error(`Error: ${err.message}`);
+  });
 };
 
 await calculateHash();
