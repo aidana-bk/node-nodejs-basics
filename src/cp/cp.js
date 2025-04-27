ce90e259f2c9c444f94f14cfe23f8a6ea432d552
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 21:56:23 +0500

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = "src/cp/files/script.js";
+  const child = spawn("node", [scriptPath, ...args]);
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+  child.stdin.on("error", (err) => {
+    console.error(err.message);
+  });
+  child.stdout.on("error", (err) => {
+    console.error(err.message);
+  });
+  child.on("error", (err) => {
+    console.error(err.message);
+  });
+  child.on("close", (code) => {
+    console.log(`Exit with ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["Hi", "my", "name", "is", "Aidana"]);
