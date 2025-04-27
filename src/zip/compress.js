54b9ca07e1634966e82503cca8834643eaacf74a
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 21:32:16 +0500

    feat: zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const inputFilePath = "src/zip/files/fileToCompress.txt";
+  const outputFilePath = "src/zip/files/archive.gz";
+  try {
+    const readableStream = fs.createReadStream(inputFilePath);
+    const writableStream = fs.createWriteStream(outputFilePath);
+    const gzipStream = zlib.createGzip();
+    readableStream.pipe(gzipStream).pipe(writableStream);
+    readableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    gzipStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    writableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    writableStream.on("finish", () => {
+      console.log("Finish");
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await compress();
