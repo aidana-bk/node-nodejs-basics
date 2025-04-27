73151f3b19ba107e02884c1695b0bb6a6b8a0101
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 21:32:38 +0500

    feat: zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const inputFilePath = "src/zip/files/archive.gz";
+  const outputFilePath = "src/zip/files/fileToCompress.txt";
+  try {
+    const readableStream = fs.createReadStream(inputFilePath);
+    const writableStream = fs.createWriteStream(outputFilePath);
+    const gunzipStream = zlib.createGunzip();
+    readableStream.pipe(gunzipStream).pipe(writableStream);
+    readableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    gunzipStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    writableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+    writableStream.on("finish", () => {
+      console.log("Finish");
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await decompress();
