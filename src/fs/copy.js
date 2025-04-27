d1e1807a72933e87acc7b575d60d58ab1ede9e81
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 16:32:22 +0500

    feat: fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  const originPath = "src/fs/files";
+  const copyPath = "src/fs/files_copy";
+  fs.cp(originPath, copyPath, { recursive: true })
+    .then(() => {
+      console.log("Copied from files to files_copy :)");
+    })
+    .catch((err) => {
+      console.error("FS operation failed");
+    });
 };
 
 await copy();
