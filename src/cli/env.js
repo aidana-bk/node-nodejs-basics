54df7ec5da924918f8029867bba97bba8afb84f9
Aidana K <aidana.amnbk@gmail.com>
2025-04-27 17:11:31 +0500

    feat: cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = [];
+  for (const key in process.env) {
+    if (key.startsWith("RSS_")) {
+      const formattedString = `${key}=${process.env[key]}`;
+      envVars.push(formattedString);
+    }
+  }
+  console.log(envVars.join("; "));
 };
 
 parseEnv();
